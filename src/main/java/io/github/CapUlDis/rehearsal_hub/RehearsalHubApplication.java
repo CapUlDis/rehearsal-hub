63b9a3d5cd01be788532c9c65ac10eae5f0eaa85
Дубков Денис <d.dubkov@vk.team>
2025-05-29 08:38:36 +0300

    Initial commit

---
+++ src/main/java/io/github/CapUlDis/rehearsal_hub/RehearsalHubApplication.java
@@ -0,0 +1,13 @@
+package io.github.CapUlDis.rehearsal_hub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RehearsalHubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RehearsalHubApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/CapUlDis/rehearsal_hub/RehearsalHubApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.CapUlDis.rehearsal_hub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RehearsalHubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
